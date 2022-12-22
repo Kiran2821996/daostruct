57a26b467d1e894d1e72ee6efcde730ad5fcfe24
Kiran2821996 <kirandchennai@gmail.com>
2022-12-22 18:26:02 +0530

    add

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 import video from "./assets/video.mp4"
+import './index.css';
 
 
 
